ef26fad982c9b40e923c92e7eaac54e428b84be6
Matt Lyon <matthewlyon18@gmail.com>
2023-04-12 19:42:13 +0100

    moved to react-bootstrap-icon for mail icons

--- client/src/components/MailDropdown.tsx
+++ client/src/components/MailDropdown.tsx
@@ -5,37 +5,8 @@ import { Dispatch, SetStateAction } from 'react';
 import { Message } from '../utils/types';
 import { getConfig } from '../utils/auth';
 import { UserContext } from '../context/UserContext';
-
-function UnreadMailIcon() {
-    return (
-        <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='2em'
-            height='2em'
-            fill='currentColor'
-            viewBox='0 0 16 16'
-            className='hvr-grow3'
-        >
-            <path d='M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 4.697v4.974A4.491 4.491 0 0 0 12.5 8a4.49 4.49 0 0 0-1.965.45l-.338-.207L16 4.697Z' />
-            <path d='M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1.5a.5.5 0 0 1-1 0V11a.5.5 0 0 1 1 0Zm0 3a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z' />
-        </svg>
-    );
-}
-
-function MailIcon() {
-    return (
-        <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='2em'
-            height='2em'
-            fill='currentColor'
-            viewBox='0 0 16 16'
-            className='hvr-grow3'
-        >
-            <path d='M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z' />
-        </svg>
-    );
-}
+import { EnvelopeFill } from 'react-bootstrap-icons';
+import { EnvelopeExclamationFill } from 'react-bootstrap-icons';
 
 function NotificationCircle(props) {
     const { color, className } = props;
@@ -187,7 +158,13 @@ export function MailDropdown(props: MailDropdownProps) {
 
     return (
         <NavDropdown
-            title={numUnread === 0 ? MailIcon() : UnreadMailIcon()}
+            title={
+                numUnread === 0 ? (
+                    <EnvelopeFill size='2em' />
+                ) : (
+                    <EnvelopeExclamationFill size='2em' />
+                )
+            }
             id='notifications'
             align='end'
             className='notification'
